32db9b5be049ceada00b825f4424ae4d002b281e
David <davidwdiniz@gmail.com>
2022-04-05 10:16:08 -0300

    Prisma configuration

---
+++ purchases/src/http/auth/test.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { PrismaService } from '../../database/prisma/prisma.service';
+
+@Controller('test')
+export class TestController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get('ok')
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return this.prisma.customer.findMany();
+  }
+}
