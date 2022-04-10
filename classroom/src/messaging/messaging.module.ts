bead54eb6593ea2ec4c08b3e45e87db21e36d3dc
David <davidwdiniz@gmail.com>
2022-04-10 18:50:17 -0300

    Kafka microservices purchase to classroom

--- classroom/src/messaging/messaging.module.ts
+++ classroom/src/messaging/messaging.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CoursesService } from 'src/services/courses.service';
+import { StudentsService } from 'src/services/students.service';
 import { PurchasesController } from './controllers/purchases.controller';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchasesController],
+  providers: [StudentsService, CoursesService, EnrollmentsService],
 })
 export class MessagingModule {}
